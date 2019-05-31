752635187ecbe2e006d5c1499ae741b975c36c83
Duong Vi Phat <viphat@gmail.com>
2019-05-31 15:57:22 +0900

    Fix createUser Mutation & Add some arguments description

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,13 +1,14 @@
 class Mutations::CreateUser < Mutations::BaseMutation
-  argument :email, String, required: true
-  argument :name, String, required: true
-  argument :password, String, required: true
+  description 'Create User'
+  argument :email, String, 'Email that will be used to sign in (must be unique)', required: true
+  argument :name, String, "User's name", required: true
+  argument :password, String, 'Password used to sign in (At least 6 characters)', required: true
 
   field :user, Types::UserType, null: true
   field :errors, [String], null: false
 
-  def resolve(name:, email:)
-    user = User.new(name: name, email: email)
+  def resolve(name:, email:, password:)
+    user = User.new(name: name, email: email, password: password)
     if user.save
       return {
         user: user,
